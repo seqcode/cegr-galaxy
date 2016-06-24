4602773457f36fe29ac017c970b0726e782a676d
Greg Von Kuster <ghv2@psu.edu>
2016-06-24 15:32:04 -0400

    Fix statistics tools for tools that produce multiple outputs so that PEGR will be able to properly store information using the tool_id as the identifier.

--- tools/cegr_statistics/cwpair2_output_stats.py
+++ tools/cegr_statistics/cwpair2_output_stats.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import argparse
-import os
 import stats_util
 
 STATS = ['peakPairWis']
@@ -10,23 +9,28 @@ parser.add_argument('--config_file', dest='config_file', help='stats_config.ini'
 parser.add_argument('--history_id', dest='history_id', help='History id')
 parser.add_argument('--history_name', dest='history_name', help='History name')
 parser.add_argument('--input', dest='inputs', action='append', nargs=5, help='Input datasets and attributes')
+parser.add_argument('--output', dest='output', help='Output dataset')
 parser.add_argument('--tool_id', dest='tool_id', help='Tool that was executed to produce the input dataset')
 parser.add_argument('--tool_parameters', dest='tool_parameters', help='Tool parameters that were set when producing the input dataset')
 args = parser.parse_args()
 
-os.mkdir('output')
+payload = None
+statistics = []
+datasets = []
+# Generate the statistics and datasets.
 for input in args.inputs:
     file_path, hid, input_id, input_datatype, dbkey = input
-    output_name = os.path.join('output', 'statistics_on_data_%s.txt' % hid)
-    # Initialize the payload.
-    payload = stats_util.get_base_json_dict(args.config_file, dbkey, args.history_id, args.history_name, args.tool_id, args.tool_parameters)
-    # Generate the statistics and datasets.
-    payload['statistics'] = stats_util.get_statistics(file_path, STATS)
-    payload['datasets'] = stats_util.get_datasets(args.config_file, input_id, input_datatype)
-    # Send the payload to PEGR.
-    pegr_url = stats_util.get_pegr_url(args.config_file)
-    response = stats_util.submit(args.config_file, payload)
-    # Make sure all is well.
-    stats_util.check_response(pegr_url, payload, response)
-    # If all is well, store the results in the output.
-    stats_util.store_results(output_name, pegr_url, payload, response)
+    if payload is None:
+        # Initialize the payload.
+        payload = stats_util.get_base_json_dict(args.config_file, dbkey, args.history_id, args.history_name, args.tool_id, args.tool_parameters)
+    statistics.append(stats_util.get_statistics(file_path, STATS))
+    datasets.append(stats_util.get_datasets(args.config_file, input_id, input_datatype))
+payload['statistics'] = statistics
+payload['datasets'] = datasets
+# Send the payload to PEGR.
+pegr_url = stats_util.get_pegr_url(args.config_file)
+response = stats_util.submit(args.config_file, payload)
+# Make sure all is well.
+stats_util.check_response(pegr_url, payload, response)
+# If all is well, store the results in the output.
+stats_util.store_results(args.output, pegr_url, payload, response)
