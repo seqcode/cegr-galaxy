f062bde900e6dde8ed9dde8c87c4ff67d6f09595
Greg Von Kuster <ghv2@psu.edu>
2016-07-07 08:37:22 -0400

    Add the HTML meme output to the statisitcs for PEGR

--- tools/cegr_statistics/meme_meme_output_stats.py
+++ tools/cegr_statistics/meme_meme_output_stats.py
@@ -6,21 +6,23 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--config_file', dest='config_file', help='stats_config.ini')
 parser.add_argument('--history_id', dest='history_id', help='History id')
 parser.add_argument('--history_name', dest='history_name', help='History name')
-parser.add_argument('--input', dest='inputs', action='append', nargs=5, help='Input datasets and attributes')
+parser.add_argument('--input_html', dest='input_htmls', action='append', nargs=5, help='HTML input datasets and attributes')
+parser.add_argument('--input_txt', dest='input_txts', action='append', nargs=5, help='Text input datasets and attributes')
 parser.add_argument('--output', dest='output', help='Output dataset')
 parser.add_argument('--tool_id', dest='tool_id', help='Tool that was executed to produce the input dataset')
 parser.add_argument('--tool_parameters', dest='tool_parameters', help='Tool parameters that were set when producing the input dataset')
 args = parser.parse_args()
 
-payload = None
+payload = stats_util.get_base_json_dict(args.config_file, dbkey, args.history_id, args.history_name, args.tool_id, args.tool_parameters)
 statistics = []
 datasets = []
 # Generate the statistics and datasets.
-for input in args.inputs:
-    file_path, hid, input_id, input_datatype, dbkey = input
-    if payload is None:
-        # Initialize the payload.
-        payload = stats_util.get_base_json_dict(args.config_file, dbkey, args.history_id, args.history_name, args.tool_id, args.tool_parameters)
+for input_html in args.input_htmls:
+    file_path, hid, input_id, input_datatype, dbkey = input_html
+    statistics.append({})
+    datasets.append(stats_util.get_datasets(args.config_file, input_id, input_datatype))
+for input_txt in args.input_txts:
+    file_path, hid, input_id, input_datatype, dbkey = input_txt
     statistics.append({})
     datasets.append(stats_util.get_datasets(args.config_file, input_id, input_datatype))
 payload['statistics'] = statistics
