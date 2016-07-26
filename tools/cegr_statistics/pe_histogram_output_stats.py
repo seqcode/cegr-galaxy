fc8b58a3ccd6d0608bb570318544da991a1c7360
Greg Von Kuster <ghv2@psu.edu>
2016-07-26 18:43:36 -0400

    Actually store the pe_hitogram statistics after calculating them

--- tools/cegr_statistics/pe_histogram_output_stats.py
+++ tools/cegr_statistics/pe_histogram_output_stats.py
@@ -28,6 +28,7 @@ statistics = []
 statistics.append({})
 # Generate the statistics for the tabular dataset.
 statistics.append(stats_util.get_statistics(args.input_tabular, STATS))
+payload['statistics'] = statistics
 d1 = stats_util.get_datasets(args.config_file, args.input_png_id, args.input_png_datatype)
 d2 = stats_util.get_datasets(args.config_file, args.input_tabular_id, args.input_tabular_datatype)
 payload['datasets'] = [d1, d2]
